96e2ab8aff5aef2276fe9fbfdc157e8dc4aee43e
dinagalevska <dinagalevska2002@gmail.com>
2024-11-26 14:49:16 +0100

    job for eventsim data and commands used

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,41 @@ resource "google_compute_instance" "kafka_vm_instance" {
 output "kafka_vm_ip" {
   value = google_compute_instance.kafka_vm_instance.network_interface[0].access_config[0].nat_ip
 }
+
+resource "google_cloud_run_v2_job" "default" {
+  name                = "data-generate-job"
+  location            = var.region
+  deletion_protection = false
+
+  template {
+    template {
+      containers {
+        image = "gcr.io/streamsonic-441414/streamsonic:latest"
+
+        resources {
+          limits = {
+            "memory" = "4Gi"
+          }
+        }
+
+        command = []
+
+        args = [
+          "-c", "examples/example-config.json",
+          "--start-time", "2021-01-01T00:00:00",
+          "--end-time", "2021-12-01T00:00:00",
+          "--nusers", "100",
+          "--kafkaBrokerList", "35.239.250.116:9092"
+        ]
+      }
+
+      timeout = "600s" # Adjust the timeout as needed
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].template[0].containers[0].resources[0].limits
+    ]
+  }
+}
